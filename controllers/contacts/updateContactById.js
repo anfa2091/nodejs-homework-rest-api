c56d6120c178963c02c6e0baebdbb57b9652546a
Antony Córdoba <anfa2091@gmail.com>
2023-10-23 21:53:39 -0500

    Workr finish

--- controllers/contacts/updateContactById.js
+++ controllers/contacts/updateContactById.js
@@ -1,18 +1,19 @@
-const { basedir } = global;
-const { Contact } = require(`${basedir}/models/contact`);
-const { createError } = require(`${basedir}/helpers`);
-
+const ContactModel = require('../../models/contact');
+const { createError } = require('../../helpers');
+require("dotenv").config();
 const updateContactById = async (req, res, next) => {
-    try {
-      const { contactId } = req.params;
-        const result = await Contact.findByIdAndUpdate(contactId, req.body );
-        if (!result) {
-          throw createError(404);
-        }
-        res.json(result);
-    } catch (error) {
-      next(error);
-    }
-  };
+  try {
+    const { contactId } = req.params;
+    const result = await ContactModel.findByIdAndUpdate(contactId, req.body, { new: true });
 
-module.exports = updateContactById;  
+    if (!result) {
+      throw createError(404);
+    }
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = updateContactById;
--- server.js
+++ server.js
@@ -1,18 +1,55 @@
-const app = require('./app');
-const mongoose = require('mongoose');
-const { DB_HOST, PORT } = process.env;
+const express = require("express");
+const cors = require("cors");
+const association = require("./db/association");
+require("dotenv").config();
+const routerApi = require("./routes/api/contacts");
 
-mongoose.set("strictQuery", true);
+const app = express();
 
-mongoose
-  .connect(DB_HOST)
-    .then(() =>
-      app.listen(PORT, () => {
-        console.log("Database connection successful");
-      })
-  )
-    
-  .catch((err) => {
-    console.log(err.message);
-    process.exit(1);  
+app.use(express.json());
+app.use(cors());
+
+require("./config/config-passport");
+
+app.use("/api", routerApi);
+
+app.use((_, res) => {
+  res.status(404).json({
+    status: "error",
+    code: 404,
+    message: ` Use api on routes:
+    /api/signup - signup user { username, email, password}
+    /api/login -login {email,password}
+    /api/list - get message if user is authenticated`,
+    data: "No found",
+  });
 });
+
+app.use((err, _, res, next) => {
+  console.log(err.stack);
+  res.status(500).json({
+    status: "fail",
+    code: 500,
+    message: err.message,
+    data: "Internal server error",
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+association
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(
+        `Database connection successful.
+         Server running. Use our API on port : ${PORT}`
+      );
+    })
+  )
+  .catch((error) => {
+    console.log(`Server not running. 
+Eror message: ${error.message}`);
+    process.exit(1);
+  });
+
+module.exports = app;
